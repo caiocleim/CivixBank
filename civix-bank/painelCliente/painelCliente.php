452bf4a8d3b00f8c01283418b90c114571067b66
caiocleim <127921036+caiocleim@users.noreply.github.com>
2025-06-26 20:31:55 -0300

    civix-bank 26-06-2025

---
+++ civix-bank/painelCliente/painelCliente.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[Cliente] - Pagina Inicial</title>
+    <link rel="stylesheet" href="./css/painelCliente.css">
+</head>
+<body>
+    <header>
+
+        <div class="up-header">
+            <a href="#">
+                <img src="./img/default.png" alt="">
+            </a>
+            
+
+        </div>
+        <div class="down-header">
+            <p>Olá, [Cliente]!</p>
+        </div>
+        <a class="sair" href="../index.php">Sair</a>
+    </header>
+    <div class="blank-space"></div>
+    <div class="body-page">
+
+            <div class="label-conta">
+                <p>Saldo Disponivel</p>
+            </div>
+
+            <div class="saldo-conta">
+                <p>R$ 1.543,00</p>
+            </div>
+
+            <div class="icons-page">
+
+                <a class="icon" href="">
+                    <img src="../icons\transferir.png" alt="">
+                    <p>Transferir</p>
+                </a>
+
+                <a class="icon" href="">
+                    <img src="../icons\boleto.png" alt="">
+                    <p>Pagar</p>
+                </a>
+
+                <a class="icon" href="">
+                    <img src="../icons\depositar.png" alt="">
+                    <p>Depositar</p>
+                </a>
+
+            </div>
+           
+
+             <div class="blank-space"></div>
+            
+
+
+
+    <footer>
+        <div class="icon-inferior">
+            <a href="">
+                <img src="../icons\home.png" alt="">
+                <p>Home</p>
+            </a>
+        </div>
+
+        <div class="icon-inferior">
+            <a href="./extratoCliente/extratoCliente.php">
+                <img src="../icons\barra-inferior-extrato.png" alt="">
+                <p>Extrato</p>
+            </a>
+        </div>
+    </footer>
+    
+</body>
+</html>
